371ee75e0ddbee90d3653a7368a6c06dde13f7e4
Tatiana Turin <tatiana.turin@gmail.com>
2022-03-31 17:50:27 -0400

    commit src

--- src/ClubMembers.java
+++ src/ClubMembers.java
@@ -14,13 +14,18 @@ public class ClubMembers {
     }
 
     public ArrayList<MemberInfo> removeMembers(int year) {
-        ArrayList<MemberInfo> graduatedList = new ArrayList<MemberInfo>;
+        ArrayList<MemberInfo> graduatedList = new ArrayList<MemberInfo>();
+
+
         for (int i = 0; i < memberList.size(); i++) {//index through memberList
+            System.out.println(memberList.get(i).getGradYear()+" ---- "+memberList.get(i).getName());
             if (memberList.get(i).getGradYear() <= year) {//if graduated
+               // System.out.println(memberList.get(i).getName());
                 if(memberList.get(i).inGoodStanding()){//if in good standing
                     graduatedList.add(memberList.get(i));//add to graduatedlist
                 }
                 memberList.remove(i);//remove bad and good standing from the memberList
+                i=i-1;
             }
         }
         return graduatedList;
@@ -31,6 +36,28 @@ public class ClubMembers {
         MemberInfo m2 = new MemberInfo("Fox, Steve", 2018,true);
         MemberInfo m3 = new MemberInfo("Xin, Michael",2017,false);
         MemberInfo m4 = new MemberInfo("Garcia, Maria",2020,true);
+
+       ClubMembers Club = new ClubMembers();
+       ArrayList<MemberInfo> mList = new ArrayList<MemberInfo>();
+       Club.memberList=mList;
+
+       Club.memberList.add(m1);
+       Club.memberList.add(m2);
+       Club.memberList.add(m3);
+       Club.memberList.add(m4);
+
+       for(int i=0;i<Club.memberList.size();i++){
+           System.out.println("Member: "+  Club.memberList.get(i).getName()+" Year: "+Club.memberList.get(i).getGradYear());
+       }
+
+       ArrayList<MemberInfo> graduatedList = Club.removeMembers(2018);
+        for(int i=0;i<Club.memberList.size();i++){
+            System.out.println("Member: "+  Club.memberList.get(i).getName()+" Year: "+Club.memberList.get(i).getGradYear());
+        }
+System.out.println("------------");
+        for(int i=0;i< graduatedList.size();i++){
+            System.out.println("Member: "+  graduatedList.get(i).getName()+" Year: "+graduatedList.get(i).getGradYear());
+        }
 
 
     }
