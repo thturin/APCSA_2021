4505e2314aac1e11b4a104e24097df811567cf1d
tatiana <tatiana.turin@gmail.com>
2022-03-31 12:27:12 -0400

    commit meow

--- src/CombinedTable.java
+++ src/CombinedTable.java
@@ -1,9 +1,46 @@
-public class CombinedTable extends SingleTable {
+public class CombinedTable  {
     private SingleTable x;
     private SingleTable y;
 
-    public int canSeat(){
-        int num = (x.getNumSeats()+y.getNumSeats())-2;
-        return num;
+    public CombinedTable(SingleTable s1, SingleTable s2){
+        x=s1;
+        y=s2;
     }
+    public boolean canSeat(int seats){
+        int maxSeats = (x.getNumSeats()+y.getNumSeats())-2;
+        boolean ans=false;
+        if (seats<=maxSeats){
+            ans=true;
+        }
+        return ans;
+    }
+    public double getDesirability(){
+        double ans = (x.getViewQuality()+y.getViewQuality())/2.0;
+        //System.out.println("ans="+ans);
+        if(x.getHeight()==y.getHeight()){
+            //System.out.println("This is the desirability = "+ans);
+        }else{
+            ans=ans-10;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        SingleTable t1 = new SingleTable(4,60.0,74);
+        SingleTable t2 = new SingleTable(8,70.0,74);
+        SingleTable t3 = new SingleTable(12,75.0,76);
+
+        CombinedTable c1 = new CombinedTable(t1, t2);
+        //System.out.println(c1.canSeat(11));
+        //System.out.println(c1.getDesirability());
+
+        CombinedTable c2 = new CombinedTable(t2,t3);
+        //System.out.println(c2.canSeat(18));
+        //System.out.println(c2.getDesirability());
+        t2.setViewQuality(80);
+        //System.out.println(t2.getViewQuality());
+        System.out.println(c2.getDesirability());
+    }
+
+
 }
+
--- src/SingleTable.java
+++ src/SingleTable.java
@@ -1,7 +1,13 @@
 public class SingleTable {
     private int seats;
-    private int height;
     private double view;
+    private int height;
+
+    public SingleTable(int s, double v, int h){
+        seats=s;
+        view=v;
+        height=h;
+    }
     //Returns the number of seats at this table. The value is always greater than or equal to 4
     public int getNumSeats(){return seats;}
     public int getHeight(){return height;}
